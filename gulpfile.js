af12659108574f5a2f6f99786fb4a6c7a19c4ffa
Drews8 <prokhorenko.drew@gmail.com>
2020-01-31 01:14:06 +0200

    change gulp config file

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,47 @@
-function defaultSomeTask(done) {
-    console.log("All is working");
+const gulp = require('gulp'),
+    rename = require('gulp-rename'),
+    scss = require('gulp-sass'),
+    autoprefixer = require('gulp-autoprefixer'),
+    sourcemaps = require('gulp-sourcemaps'),
+    browserSync = require('browser-sync').create();
+
+function cssStyle(done) {
+    gulp.src('scss/**/*.scss')
+        .pipe(sourcemaps.init())
+        .pipe(scss({
+            errorLogToConsole: true,
+            outputStyle: 'compressed'
+        }).on('error', console.error.bind(console)))
+        .pipe(autoprefixer({
+            cascade: false
+        }))
+        .pipe(rename({suffix: '.min'}))
+        .pipe(sourcemaps.write('./'))
+        .pipe(gulp.dest('css/'))
+        .pipe(browserSync.stream());
+
     done();
 }
 
+function sync(done) {
+    browserSync.init({
+        server: {
+            baseDir: './'
+        },
+        port: 3000
+    });
+    done();
+}
+function browserReload(done) {
+    browserSync.reload();
+    done();
+}
+function watchFiles() {
+    gulp.watch('./scss/**/*', cssStyle);
+    gulp.watch('**/*.html', browserReload);
+    gulp.watch('**/*.js', browserReload);
+}
 
-exports.default = defaultSomeTask;
+
+
+gulp.task('default', gulp.parallel(watchFiles, sync));
